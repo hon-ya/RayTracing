04cd17f19b78728058f3cc6e64e984f438ec049f
hon_ya <spaceodyssey2008@gmail.com>
2017-10-30 22:14:16 +0900

    Ray Tracing in One Weekend: Chapture1: Output an image のコードを実装

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace RayTracing
     {
         static void Main(string[] args)
         {
+            var builder = new StringBuilder();
+
+            var nx = 200;
+            var ny = 100;
+
+            builder.AppendFormat($"P3\n");
+            builder.AppendFormat($"{nx} {ny}\n");
+            builder.AppendFormat($"255\n");
+
+            for (var j = ny - 1; j >= 0; j--)
+            {
+                for (var i = 0; i < nx; i++)
+                {
+                    var r = 1.0 * i / nx;
+                    var g = 1.0 * j / ny;
+                    var b = 0.2;
+                    var ir = (int)(255.99 * r);
+                    var ig = (int)(255.99 * g);
+                    var ib = (int)(255.99 * b);
+
+                    builder.AppendFormat($"{ir} {ig} {ib}\n");
+                }
+            }
+
+            File.WriteAllText("output.ppm", builder.ToString());
         }
     }
 }
