d2d635520b0fff6cda2c4d59a9783ff4f5c5e564
hon_ya <spaceodyssey2008@gmail.com>
2017-11-04 15:07:51 +0900

    Ray Tracing in One Weekend: Chapture7: Diffuse Materials

--- Program.cs
+++ Program.cs
@@ -45,10 +45,12 @@ namespace RayTracing
                         var v = (1.0f * j + random.NextFloat(0.0f, 1.0f)) / ny;
 
                         var ray = camera.GetRay(u, v);
-                        color += GetColor(ray, world);
+                        color += GetColor(ray, world, random);
                     }
 
                     color = Vector3.Divide(color, ns);
+                    color = new Vector3((float) Math.Sqrt(color.Red), (float) Math.Sqrt(color.Green),
+                        (float) Math.Sqrt(color.Blue));
 
                     var ir = (int)(255.99f * color.Red);
                     var ig = (int)(255.99f * color.Green);
@@ -61,20 +63,32 @@ namespace RayTracing
             File.WriteAllText("output.ppm", builder.ToString());
         }
 
-        private static Color3 GetColor(Ray ray, HitableList world)
+        private static Color3 GetColor(Ray ray, HitableList world, Random random)
         {
-            var result = world.Hit(ray, 0.0f, float.MaxValue);
+            var result = world.Hit(ray, 0.001f, float.MaxValue);
             if (result.HasValue)
             {
                 var record = result.Value;
-
-                return 0.5f * Color3.Add(record.Normal, Color3.White);
+                var target = record.Position + record.Normal + RandomInUnitSphere(random);
+                return 0.5f * GetColor(new Ray(record.Position, target - record.Position), world, random);
             }
             else
             {
                 var directionUnit = Vector3.Normalize(ray.Direction);
                 var t = 0.5f * (directionUnit.Y + 1.0f);
                 return (1.0f - t) * Color3.White + t * new Color3(0.5f, 0.7f, 1.0f);
+            }
+        }
+
+        private static Vector3 RandomInUnitSphere(Random random)
+        {
+            while (true)
+            {
+                var p = 2.0f * random.NextVector3(Vector3.Zero, Vector3.One) - Vector3.One;
+                if (p.Length() < 1.0f)
+                {
+                    return p;
+                }
             }
         }
     }
