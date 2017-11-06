cda507b66d0c9c63c3c987768e4cefc36ae92489
hon_ya <spaceodyssey2008@gmail.com>
2017-11-06 23:01:08 +0900

    Ray Tracing in One Weekend: Chapture10: Positionable camera
    fov 実装

--- Camera.cs
+++ Camera.cs
@@ -8,17 +8,23 @@ namespace RayTracing
         public Vector3 LowerLeftCorner { get; private set; }
         public Vector3 Horizontal { get; private set; }
         public Vector3 Vertical { get; private set; }
-        public Vector3 Origin { get; private set; } = new Vector3(0.0f);
+        public Vector3 Origin { get; private set; }
 
-        public Camera(float vfov, float aspect)
+        public Camera(Vector3 lookFrom, Vector3 lookAt, Vector3 up, float vfov, float aspect)
         {
             var theta = MathUtil.DegreesToRadians(vfov);
             var halfHeight = (float)Math.Tan(theta / 2.0f);
             var halfWidth = aspect * halfHeight;
 
-            LowerLeftCorner = new Vector3(-halfWidth, -halfHeight, -1.0f);
-            Horizontal = new Vector3(2.0f * halfWidth, 0.0f, 0.0f);
-            Vertical = new Vector3(0.0f, 2.0f * halfHeight, 0.0f);
+            Origin = lookFrom;
+
+            var w = Vector3.Normalize(lookFrom - lookAt);
+            var u = Vector3.Normalize(Vector3.Cross(Vector3.Up, w));
+            var v = Vector3.Cross(w, u);
+
+            LowerLeftCorner = Origin - halfWidth * u - halfHeight * v - w;
+            Horizontal = 2.0f * halfWidth * u;
+            Vertical = 2.0f * halfHeight * v;
         }
 
         public Ray GetRay(float u, float v)
